69b87fbe2709950ee1869911b42702c8d56e35b2
jallend1 <jallend1@gmail.com>
2020-05-11 00:56:59 -0700

    Completes basic API functionality

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-const movieResult = document.querySelector('#movieresult');
-const description = document.querySelector('#description');
-const poster = document.querySelector('#poster');
-
+const $movieResult = document.querySelector('#movieresult');
+const $description = document.querySelector('#description');
+const $poster = document.querySelector('#poster');
+const api = '824edc3661f79aedff20cdda9e6efacd';
+let totalResults;
 let page = 1;
 const url = `https://api.themoviedb.org/3/search/movie?api_key=${api}&query=Star+Trek&page=${page}`;
-const imagesBaseURL = `https://image.tmdb.org/t/p/w300`
+const imagesBaseURL = `https://image.tmdb.org/t/p/w300`;
 
-fetch(url)
-    .then(result => result.json())
-    .then(data => {
-        const resultsArray = [...data.results]
-        console.log(resultsArray)
-        const randomMovie = Math.floor(Math.random() * resultsArray.length);
-        const movie = resultsArray[randomMovie];
-        console.log(movie)
-        movieResult.textContent = movie.title;
-        poster.src = imagesBaseURL + movie.poster_path;
-        description.textContent = movie.overview
-    });
+const trekMovies = [];
+
+
+function displayMovie(movie){
+    console.log(movie);
+    $movieResult.textContent = movie.title;
+    $poster.src = imagesBaseURL + movie.poster_path;
+    $description.textContent = movie.overview
+}
+
+function pickMovie(movieArray){
+    console.log(movieArray.length);
+    const randomMovie = Math.floor(Math.random() * movieArray.length);
+    displayMovie(movieArray[randomMovie]);
+};
+
+
+function getResults(){
+    fetch(url)
+        .then(result => result.json())
+        .then(data => { 
+            totalResults = data.total_pages;
+            trekMovies.push(...data.results);
+            pickMovie(trekMovies);
+        })
+    }
+getResults();
