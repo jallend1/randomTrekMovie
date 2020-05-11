055f2f18ed0f476683f3659e0067b3cb12520e67
jallend1 <jallend1@gmail.com>
2020-05-11 14:38:59 -0700

    Completes functionality

--- app.js
+++ app.js
@@ -4,33 +4,53 @@ const $poster = document.querySelector('#poster');
 const api = '824edc3661f79aedff20cdda9e6efacd';
 let totalResults;
 let page = 1;
-const url = `https://api.themoviedb.org/3/search/movie?api_key=${api}&query=Star+Trek&page=${page}`;
+let url = `https://api.themoviedb.org/3/search/movie?api_key=${api}&query=Star+Trek&page=${page}`;
 const imagesBaseURL = `https://image.tmdb.org/t/p/w300`;
 
 const trekMovies = [];
 
+async function additionalResults(){
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${api}&query=Star+Trek&page=${page}`;
+    const response = await fetch(url);
+    const result = await response.json()
+    .then(data => { 
+        trekMovies.push(...data.results);
+    }).then(() => {
+        if(totalResults > page){
+            page++
+            additionalResults();
+        }
+        else{
+            pickMovie(trekMovies)
+        }
+    });
+}
 
 function displayMovie(movie){
-    console.log(movie);
     $movieResult.textContent = movie.title;
     $poster.src = imagesBaseURL + movie.poster_path;
     $description.textContent = movie.overview
 }
 
 function pickMovie(movieArray){
-    console.log(movieArray.length);
     const randomMovie = Math.floor(Math.random() * movieArray.length);
     displayMovie(movieArray[randomMovie]);
 };
 
 
-function getResults(){
-    fetch(url)
-        .then(result => result.json())
-        .then(data => { 
-            totalResults = data.total_pages;
-            trekMovies.push(...data.results);
-            pickMovie(trekMovies);
-        })
-    }
+async function getResults(){
+    const response = await fetch(url);
+    const result = await response.json()
+    .then(data => { 
+        totalResults = data.total_pages;
+        trekMovies.push(...data.results);
+    }).then(() => {
+        if(totalResults > page){
+            page++;
+            additionalResults();
+        }
+    });
+}
+
 getResults();
+
