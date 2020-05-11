e1db20ed72f7cc00e628ac49adadd8c852efaee7
jallend1 <jallend1@gmail.com>
2020-05-10 18:37:59 -0700

    Removes API

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const movieResult = document.querySelector('#movieresult');
 const description = document.querySelector('#description');
 const poster = document.querySelector('#poster');
-const api = '824edc3661f79aedff20cdda9e6efacd';
+
 let page = 1;
 const url = `https://api.themoviedb.org/3/search/movie?api_key=${api}&query=Star+Trek&page=${page}`;
 const imagesBaseURL = `https://image.tmdb.org/t/p/w300`
